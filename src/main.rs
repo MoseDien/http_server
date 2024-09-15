828317ce4709e9ce7de8b79b1dfaa500370c51f2
Bell <dienbell@gmail.com>
2024-09-15 10:36:46 +0800

    Add simple log

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
-use warp::{Filter, Rejection, Reply};
+use warp::{Filter, Reply};
 use std::convert::Infallible;
 
 async fn handle_404() -> Result<impl Reply, Infallible> {
     Ok(warp::reply::html("<h1>404 - Not Found</h1><p>Welcome to unknown world!</p>"))
 }
-    
+
 #[tokio::main]
 async fn main() {
     // GET /
     let hello_world = warp::path::end().map(|| "Hello, World at root!");
 
-    // GET /hi
-    let hi = warp::path("hi").map(|| "Hello, World!");
+    // GET /rust
+    let hi = warp::path("rust").map(|| "Hello, Rust!");
 
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-    .map(|name| format!("Hello, {}!", name));
+    // GET /rusp/warp => 200 OK with body "Rust, warp!"
+    let hello = warp::path!("rust" / String)
+        .map(|name| format!("Rust, {}!", name));
+    // 添加访问日志功能
+    let log = warp::log::custom(|info| {
+        println!("{} {} {} {}",
+            info.method(),
+            info.path(),
+            info.status(),
+            info.elapsed().as_millis()
+        );
+    });
 
-    
-    let routes = warp::get().and(
-        hello_world
-        .or(hello)
-        .or(hi)
-        .or(warp::any().and_then(handle_404))
-    );
-
+    let routes = warp::get()
+        .and(
+            hello_world
+                .or(hello)
+                .or(hi)
+                .or(warp::any().and_then(handle_404))
+        )
+        .with(log);
+        
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
